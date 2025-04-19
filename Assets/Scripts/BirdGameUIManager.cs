d84345d6c8d76efc2714369ad30d623660b0e16e
armonamini <97136449+armonamini@users.noreply.github.com>
2025-04-19 12:25:07 -0700

    Updated UI for first 3 pages

--- Assets/Scripts/BirdGameUIManager.cs
+++ Assets/Scripts/BirdGameUIManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class BirdGameUIManager : MonoBehaviour
 {
@@ -10,11 +11,19 @@ public class BirdGameUIManager : MonoBehaviour
     [SerializeField] private GameObject page3; //Actual Game
     [SerializeField] private GameObject page4; //Results
     [SerializeField] private GameObject page5; //Reward Page
+    [SerializeField] private GameObject CG;
+    [SerializeField] private GameObject CW;
+    [SerializeField] private GameObject CQ;
+    [SerializeField] private GameObject RR;
+    [SerializeField] private GameObject TH;
+    [SerializeField] private GameObject ST;
+    [SerializeField] private GameObject BP;
 
     [Header("Page 3 Elements")]
     [SerializeField] private Text timerText;
     [SerializeField] private Button countButton;
-    [SerializeField] private float gameDuration = 10f; // 10 seconds game duration
+    [SerializeField] private Image timerCircle;
+    [SerializeField] private float gameDuration = 10f; // 10 seconds game duration FOR NOW
 
     [Header("Page 4 Elements")]
     [SerializeField] private Text resultText;
@@ -32,6 +41,11 @@ public class BirdGameUIManager : MonoBehaviour
     void Start()
     {
         // Show page 1 and hide others at start.
+        ShowPage(1);
+    }
+
+    public void OnBack()
+    {
         ShowPage(1);
     }
 
@@ -88,6 +102,14 @@ public class BirdGameUIManager : MonoBehaviour
         page3.SetActive(false);
         page4.SetActive(false);
         page5.SetActive(false);
+        CG.SetActive(false);
+        CW.SetActive(false);
+        CQ.SetActive(false);
+        RR.SetActive(false);
+        TH.SetActive(false);
+        ST.SetActive(false);
+        BP.SetActive(false);
+
 
         // Activate the requested page.
         switch (pageNumber)
@@ -125,13 +147,23 @@ public class BirdGameUIManager : MonoBehaviour
         while (currentTime > 0)
         {
             currentTime -= Time.deltaTime;
+
+            // Update the timer text
             timerText.text = Mathf.Ceil(currentTime).ToString();
+
+            // Update the circular fill
+            timerCircle.fillAmount = currentTime / gameDuration;
+
             yield return null;
         }
 
+        // Final state
+        timerText.text = "0 seconds";
+        timerCircle.fillAmount = 0f;
         gameRunning = false;
         EndBirdGame();
     }
+
 
     private void EndBirdGame()
     {
@@ -140,4 +172,76 @@ public class BirdGameUIManager : MonoBehaviour
         resultText.text = "You counted: " + countBirdCall + " calls!";
         ShowPage(4);
     }
+
+    public void OnCaliforniaGnatcatcher()
+    {
+        ShowBirdPage(1);
+    }
+
+    public void OnCactusWren()
+    {
+        ShowBirdPage(2);
+    }
+    public void OnCaliforniaQuail()
+    {
+        ShowBirdPage(3);
+    }
+    public void OnRoadRunner()
+    {
+        ShowBirdPage(4);
+    }
+    public void OnTailedHawk()
+    {
+        ShowBirdPage(5);
+    }
+
+    public void OnSpottedTowhee()
+    {
+        ShowBirdPage(6);
+    }
+
+    public void OnBlackPheobe()
+    {
+        ShowBirdPage(7);
+    }
+
+
+    public void ShowBirdPage(int pageNumber)
+    {
+        CG.SetActive(false);
+        CW.SetActive(false);
+        CQ.SetActive(false);
+        RR.SetActive(false);
+        TH.SetActive(false);
+        ST.SetActive(false);
+        BP.SetActive(false);
+
+        switch (pageNumber)
+        {
+            case 1:
+                CG.SetActive(true);
+                break;
+            case 2:
+                CW.SetActive(true);
+                break;
+            case 3:
+                CQ.SetActive(true);
+                break;
+            case 4:
+                RR.SetActive(true);
+                break;
+            case 5:
+                TH.SetActive(true);
+                break;
+            case 6:
+                ST.SetActive(true);
+                break;
+            case 7:
+                BP.SetActive(true);
+                break;
+
+        }
+    }
+
+
 }

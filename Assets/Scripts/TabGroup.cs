b8b2cfcd7507dac6421b7b8333d12ce7b74e41b0
SusLiu03 <susannl5@uci.edu>
2025-04-24 00:16:44 -0700

    created the new collection ui with tab system implemented

---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public Color tabIdle;
+    public Color tabHover;
+    public Color tabActive;
+    public TabButton selectedTab;
+    public List<GameObject> objectsToSwap;
+    public void Subscribe(TabButton button)
+    {
+        if (tabButtons == null)
+        {
+            tabButtons = new List<TabButton>();
+
+        }
+
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button)
+    {
+        ResetTabs();
+        if (selectedTab == null || button != selectedTab)
+        {
+            button.GetComponent<RectTransform>().anchoredPosition = new Vector2(
+                button.GetComponent<RectTransform>().anchoredPosition.x,
+                -100f
+            );
+        }
+    }
+
+    public void OnTabExit(TabButton button)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button)
+    {
+        selectedTab = button;
+        ResetTabs();
+        button.GetComponent<RectTransform>().anchoredPosition = new Vector2(
+            button.GetComponent<RectTransform>().anchoredPosition.x,
+            -100f
+        );
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < objectsToSwap.Count; i++)
+        {
+            if (i == index)
+            {
+                objectsToSwap[i].SetActive(true);
+            }
+            else
+            {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+    public void ResetTabs()
+    {
+        foreach (TabButton button in tabButtons)
+        {
+            if (selectedTab != null && button == selectedTab)
+            {
+                continue;
+            }
+            button.GetComponent<RectTransform>().anchoredPosition = new Vector2(
+                button.GetComponent<RectTransform>().anchoredPosition.x,
+                -10f
+            );
+        }
+    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}

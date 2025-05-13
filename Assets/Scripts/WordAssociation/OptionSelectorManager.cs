041c136337e47860b09a30a30e6b39076136edaa
zackthomas1 <zackthomas13@gmail.com>
2025-05-13 11:23:10 -0700

    Complete word association UI update.

--- Assets/Scripts/WordAssociation/OptionSelectorManager.cs
+++ Assets/Scripts/WordAssociation/OptionSelectorManager.cs
@@ -5,11 +5,17 @@ using UnityEngine.UI;
 
 public class OptionSelectorManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Cloud Objects")]
+    [SerializeField] private GameObject CloudImage;
+    [SerializeField] private GameObject CloudIcon;
+    [SerializeField] private GameObject CloudText;
+
     [Header("Cloud State Sprites")]
     [SerializeField] private Sprite cloudSprite;
     [SerializeField] private Sprite cloudCorrectSprite;
     [SerializeField] private Sprite cloudWrongSprite;
+    [SerializeField] private Sprite GreenCheckSprite;
+    [SerializeField] private Sprite RedXSprite;
 
     private string descriptor;
     private bool isSelected;
@@ -25,12 +31,13 @@ public class OptionSelectorManager : MonoBehaviour
         isSelected = false;
 
         // Set default text mesh state
-        TextMeshProUGUI textComponent = transform.Find("Button/Text")?.GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI textComponent = CloudText?.GetComponent<TextMeshProUGUI>();
         Debug.Assert(textComponent != null, "TextMeshProUGUI component not found on child named 'Text'.");
         if (textComponent == null ) return;
 
         textComponent.text = descriptor;
         textComponent.fontStyle = FontStyles.Normal;
+
 
         // Enable auto-sizing
         textComponent.enableAutoSizing = true;
@@ -59,27 +66,37 @@ public class OptionSelectorManager : MonoBehaviour
     private void SetSelectTextStyle(bool isSelectionCorrect)
     {
         // Set Cloud image
-        Image cloudImageComponent = transform.Find("Button/CloudImage").GetComponent<Image>();
+        Image cloudImageComponent = CloudImage?.GetComponent<Image>();
+        Debug.Assert(cloudImageComponent != null, "Image component not found on child named 'CloudImage'.");
+        if (cloudImageComponent == null) return;
+
+        // Set Cloud Icon image
+        Image cloudIconComponent = CloudIcon?.GetComponent<Image>();
         Debug.Assert(cloudImageComponent != null, "Image component not found on child named 'CloudImage'.");
         if (cloudImageComponent == null) return;
 
         if (isSelectionCorrect)
         {
             cloudImageComponent.sprite = cloudCorrectSprite;
+            CloudIcon.SetActive(true);
+            cloudIconComponent.sprite = GreenCheckSprite;
         }
         else
         {
             cloudImageComponent.sprite = cloudWrongSprite;
+            CloudIcon.SetActive(true);
+            cloudIconComponent.sprite = RedXSprite;
         }
     }
 
     private void SetDeselectTextStyle()
     {
         // Set Cloud image
-        Image cloudImageComponent = transform.Find("Button/CloudImage").GetComponent<Image>();
+        Image cloudImageComponent = CloudImage?.GetComponent<Image>();
         Debug.Assert(cloudImageComponent != null, "Image component not found on child named 'CloudImage'.");
         if (cloudImageComponent == null) return;
 
         cloudImageComponent.sprite = cloudSprite;
+        CloudIcon.SetActive(false);
     }
 }
--- Assets/Scripts/WordAssociation/WordAssociationUIManager.cs
+++ Assets/Scripts/WordAssociation/WordAssociationUIManager.cs
@@ -69,13 +69,25 @@ public class WordAssociationUIManager : MonoBehaviour
             return $"{string.Join(", ", allButLast)}, and {last}";
         }
 
-        // Generate text component content
-        TextMeshProUGUI textComponent = summaryText.GetComponent<TextMeshProUGUI>();
+        // Generate title text component content
+        TextMeshProUGUI textComponent = summaryText?.GetComponent<TextMeshProUGUI>();
         Debug.Assert(textComponent != null, $"TextMeshProUGUI component not found on child named '{summaryText.GetHierarchyPath()}'.");
         if (textComponent == null) return;
 
         string traits = FormatList(currentSpecies.traits);
         textComponent.text = $"You got it! the {currentSpecies.commonName} leaves are {traits}. Now the sprouts can finish their sign.";
+
+        // Get Body transform container
+        Transform container = transform.Find("SummaryPanel/VerticalPanel/BodyPanel/VerticalPanel");
+        Debug.Assert(container != null, "Container Panel GameObject not found on Canvas");
+        if (container == null) return;
+
+        // Instantiate and initialize the OptionSelector
+        for (int i = 0; i < currentSpecies.traits.Count; i++)
+        {
+            GameObject instance = Instantiate(optionSelectorPrefab, container.transform, false);
+            instance.GetComponent<OptionSelectorManager>().Initialize(currentSpecies.traits[i], this);
+        }
     }
 
     public void ShowHelpPanel()
@@ -103,6 +115,8 @@ public class WordAssociationUIManager : MonoBehaviour
 
         Debug.Log("[" + String.Join(", ", selections) + "]");
 
+
+        // Check if user has selected all the correct traits
         bool IsSelectionsSetCorrect = new HashSet<string>(currentSpecies.traits).SetEquals(new HashSet<string>(selections));
         if (IsSelectionsSetCorrect)
         {
@@ -119,33 +133,67 @@ public class WordAssociationUIManager : MonoBehaviour
 
     private void SpawnOptionSelectors()
     {
-        Transform container = transform.Find("WordAssociationPanel/VerticalPanel/BodyPanel/CloudPanel/VerticalPanel");
+        // Get transform container
+        Transform container = transform.Find("WordAssociationPanel/VerticalPanel/BodyPanel/CloudPanel");
         Debug.Assert(container != null, "Container Panel GameObject not found on Canvas");
         if (container == null) return;
 
-        const int itemsPerRow = 3;
+        // Generate rows
         GameObject currentRow = null;
         for (int i = 0; i < options.traits.Count; i++)
         {
-            if(i % itemsPerRow == 0)
-            {
-                // start new row panel
-                currentRow = new GameObject("RowPanel", typeof(RectTransform), typeof(HorizontalLayoutGroup));
-                currentRow.transform.SetParent(container, false);
+            // start new row panel
+            currentRow = new GameObject("RowPanel", typeof(RectTransform), typeof(HorizontalLayoutGroup));
+            currentRow.transform.SetParent(container, false);
 
-                // Optional: Style row panel
-                RectTransform rowRect = currentRow.GetComponent<RectTransform>();
-                rowRect.sizeDelta = new Vector2 (0, 100);
-            
-                HorizontalLayoutGroup layout = currentRow.GetComponent<HorizontalLayoutGroup>();
-                layout.childForceExpandHeight = true;
-                layout.childForceExpandWidth = true;
-                layout.spacing = 10f;
+            // Optional: Style row panel
+            RectTransform rowRect = currentRow.GetComponent<RectTransform>();
+            rowRect.sizeDelta = new Vector2(0, 100);
+
+            HorizontalLayoutGroup layout = currentRow.GetComponent<HorizontalLayoutGroup>();
+
+            // 
+            layout.childForceExpandWidth = true;
+            layout.childControlWidth = true;
+
+            // altnerate alignment left/right based on row index
+            bool leftSide = (i % 2 == 0); 
+            layout.childAlignment = leftSide ? TextAnchor.MiddleLeft : TextAnchor.MiddleRight;
+
+            // give padding so cloud isn't flush with edge 
+            int sidePadding = 40;
+            if (leftSide)
+            {
+                layout.padding = new RectOffset(sidePadding, 0, 5, 5);
+            }
+            else
+            {
+                layout.padding = new RectOffset(0, sidePadding, 5, 5);
+            }
+
+
+            if (!leftSide)
+            {
+                AddFlexibleFiller(currentRow.transform);
             }
 
             // Instantiate and initialize the OptionSelector
             GameObject instance = Instantiate(optionSelectorPrefab, currentRow.transform, false);
-            instance.GetComponent<OptionSelectorManager>().Initialize(options.traits[i], this);            
+            instance.GetComponent<OptionSelectorManager>().Initialize(options.traits[i], this);
+
+            if (leftSide)
+            {
+                AddFlexibleFiller(currentRow.transform);
+            }
         }
+    }
+
+
+    private void AddFlexibleFiller(Transform parent)
+    {
+        GameObject filler = new GameObject("Filler", typeof(RectTransform));
+        filler.transform.SetParent(parent, false);
+        LayoutElement layoutElem =  filler.AddComponent<LayoutElement>();
+        layoutElem.flexibleWidth = 1;
     }
 }
